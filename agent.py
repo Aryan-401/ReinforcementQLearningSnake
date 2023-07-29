d55a0c94ff408679994101bf516fe07ea0bdd147
Aryan-401 <aryan.raj.garg@gmail.com>
2023-07-29 20:08:26 +0530

    implemented basic agent

--- agent.py
+++ agent.py
@@ -53,7 +53,8 @@ class Agent:
         self.epsilon = 0  # control randomness
         self.gamma = 0  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)
-        # TODO: add model
+        self.model = None  # TODO: add model
+        self.trainer = None  # TODO: add trainer
 
     def get_state(self, game):
         head = game.snake[0]
@@ -105,13 +106,32 @@ class Agent:
         self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        for state, action, reward, next_state, done in mini_sample:
+            self.train_short_memory(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        self.epsilon = random.randint(80, 200) - self.n_games
+        final_move = [0, 0, 0]
+        if random.randint(0, 350) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
 
 
 if __name__ == '__main__':
