cd1ce069d8e5922440d45aa6a22431b4a0df1d5b
Aryan-401 <aryan.raj.garg@gmail.com>
2023-07-29 22:41:02 +0530

    revert to commit be5ca5680c

--- agent.py
+++ agent.py
@@ -51,8 +51,7 @@ def train():
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
-            if agent.n_games % 10 == 0:
-                plot(plot_scores, plot_mean_scores, agent.n_games)
+            plot(plot_scores, plot_mean_scores, n_games=agent.n_games)
 
 
 
--- helper.py
+++ helper.py
@@ -14,4 +14,6 @@ def plot(scores, mean_scores, n_games):
     plt.ylim(ymin=0)
     plt.text(len(scores) - 1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores) - 1, mean_scores[-1], str(mean_scores[-1]))
-    plt.savefig(f'./plots/{n_games}.png')
+    if n_games % 10 == 1:
+        plt.savefig(f'./plots/{n_games}.png')
+    plt.show(block=False)
