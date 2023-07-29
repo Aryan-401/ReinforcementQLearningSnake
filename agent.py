ce2276d2d55f893cf71723acdb9fdabbe6841b13
Aryan-401 <aryan.raj.garg@gmail.com>
2023-07-29 21:10:11 +0530

    saving plots fixed

--- agent.py
+++ agent.py
@@ -51,8 +51,7 @@ def train():
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
-            plt.savefig('plot.png')
+            plot(plot_scores, plot_mean_scores, n_games=agent.n_games)
 
 
 
--- helper.py
+++ helper.py
@@ -3,9 +3,8 @@ from IPython import display
 
 plt.ion()
 
-def plot(scores, mean_scores):
-    display.clear_output(wait=True)
-    display.display(plt.gcf())
+
+def plot(scores, mean_scores, n_games):
     plt.clf()
     plt.title('Training...')
     plt.xlabel('Number of Games')
@@ -13,7 +12,8 @@ def plot(scores, mean_scores):
     plt.plot(scores)
     plt.plot(mean_scores)
     plt.ylim(ymin=0)
-    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.text(len(scores) - 1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores) - 1, mean_scores[-1], str(mean_scores[-1]))
+    if n_games % 10 == 1:
+        plt.savefig(f'./plots/{n_games}.png')
     plt.show(block=False)
-    plt.pause(.1)
